868376786d14f834208e0cab264c55245771f0e1
Atul Chopade <atulchopade577@gmail.com>
2024-01-27 12:44:48 +0530

    ThemeSwitcher using Context API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
-function App() {
+const App = () => {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  //actual Change in theme
+
+  useEffect(() => {
+    let htmlvar = document.querySelector('html').classList;
+    htmlvar.remove("light", "dark");
+    htmlvar.add(themeMode);
+  }, [themeMode])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* Theme Button */}
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* Theme Card  */}
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
